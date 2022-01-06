7297edd9051249616c4e4e45372f9d70c8e0bed5
Maximilian Hansen <maximilianhansen@Maximilians-MBP.lan>
2022-01-06 16:49:50 -0500

    initial commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import getEntries from "./controllers/entries.js"
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true,
     }
   });
